9480ecbb6262537bea119cb4ec6fe08efb2e2fec
Mihail Yordanov <mialskywalker@gmail.com>
2026-05-27 21:45:19 +0200

    DX12 Initialization

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -11,12 +11,75 @@ bool Renderer::Init(HWND hwnd, uint32_t width, uint32_t height)
 	m_Width = width;
 	m_Height = height;
 
-	Logger::Log("Rendered initialized successfully!");
+	bool ok = CreateFactory();
+	ok = ok && PickAdapter();
+	ok = ok && CreateDevice();
 
-	return true;
+	Logger::Log("Renderer initialization successful");
+
+	return ok;
 }
 
 void Renderer::Render()
 {
 	
+}
+
+bool Renderer::CreateFactory()
+{
+	UINT flags = 0;
+
+#if defined(_DEBUG)
+	flags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	bool ok = SUCCEEDED(CreateDXGIFactory2(flags, IID_PPV_ARGS(&m_Factory)));
+
+	if (ok)
+	{
+		Logger::Log("Created Factory");
+	}
+	else
+	{
+		Logger::Log("Factory creation FAILED");
+	}
+
+	return ok;
+}
+
+bool Renderer::PickAdapter()
+{
+	ComPtr<IDXGIAdapter1> adapter;
+
+	for (UINT index = 0; DXGI_ERROR_NOT_FOUND != m_Factory->EnumAdapters1(index, &adapter); ++index)
+	{
+		DXGI_ADAPTER_DESC1 desc;
+		adapter->GetDesc1(&desc);
+
+		if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			continue;
+
+		adapter.As(&m_Adapter);
+		Logger::Log("Compatible adapter found");
+		return true;
+	}
+
+	Logger::Log("Compatible adapter not found");
+	return false;
+}
+
+bool Renderer::CreateDevice()
+{
+	bool ok = SUCCEEDED(D3D12CreateDevice(m_Adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device)));
+	
+	if (ok)
+	{
+		Logger::Log("Created Device");
+	}
+	else
+	{
+		Logger::Log("Device creation FAILED");
+	}
+
+	return ok;
 }
--- Engine/Renderer.h
+++ Engine/Renderer.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "windows.h"
 #include <cstdint>
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include "d3dx12.h"
+#include <wrl.h>
+
+using Microsoft::WRL::ComPtr;
 
 class Renderer
 {
@@ -15,4 +21,14 @@ private:
 	HWND m_Hwnd = nullptr;
 	uint32_t m_Width = 1280;
 	uint32_t m_Height = 720;
+
+	// DirectX 12
+	ComPtr<IDXGIFactory6> m_Factory;
+	ComPtr<IDXGIAdapter4> m_Adapter;
+	ComPtr<ID3D12Device> m_Device;
+
+private:
+	bool CreateFactory();
+	bool PickAdapter();
+	bool CreateDevice();
 };
