e13cd55f9f0ca696d27592da25ec88900bec9bc5
Mihail Yordanov <mialskywalker@gmail.com>
2026-06-05 10:49:16 +0200

    added debug layer + fence + gpu sync

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -11,7 +11,13 @@ bool Renderer::Init(HWND hwnd, uint32_t width, uint32_t height)
 	m_Width = width;
 	m_Height = height;
 
-	bool ok = CreateFactory();
+	bool ok = true;
+
+#if defined(_DEBUG)
+	ok = ok && EnableDebugLayer();
+#endif
+
+	ok = ok && CreateFactory();
 	ok = ok && PickAdapter();
 	ok = ok && CreateDevice();
 	ok = ok && CreateCommandQueue();
@@ -20,15 +26,65 @@ bool Renderer::Init(HWND hwnd, uint32_t width, uint32_t height)
 	ok = ok && CreateRTV();
 	ok = ok && CreateCommandAllocators();
 	ok = ok && CreateCommandList();
+	ok = ok && CreateFence();
 
 	Logger::Log("Renderer initialization successful");
 
 	return ok;
 }
 
+void Renderer::PreRender()
+{
+	m_CommandAllocators[m_CurrentFrameIndex]->Reset();
+	m_CommandList->Reset(m_CommandAllocators[m_CurrentFrameIndex].Get(), nullptr);
+}
+
 void Renderer::Render()
 {
-	
+	// PreRender
+	PreRender();
+
+	// Render
+	CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_BackBuffers[m_CurrentFrameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	m_CommandList->ResourceBarrier(1, &barrier);
+
+	auto RTVhandle = m_RTVDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	RTVhandle.ptr += m_CurrentFrameIndex * m_RTVDescriptorSize;
+
+	float clearColor[] = { 0.1f, 0.2f, 0.4f, 1.0f };
+	m_CommandList->ClearRenderTargetView(RTVhandle, clearColor, 0, nullptr);
+
+	barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_BackBuffers[m_CurrentFrameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+	m_CommandList->ResourceBarrier(1, &barrier);
+
+	m_CommandList->Close();
+
+	ID3D12CommandList* commandLists[] = { m_CommandList.Get() };
+	m_CommandQueue->ExecuteCommandLists(1, commandLists);
+
+	// PostRender
+	PostRender();
+
+	// Flush
+	WaitForGPU();
+}
+
+void Renderer::PostRender()
+{
+	m_SwapChain->Present(1, 0);
+	m_CurrentFrameIndex = m_SwapChain->GetCurrentBackBufferIndex();
+}
+
+bool Renderer::EnableDebugLayer()
+{
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&m_DebugController))))
+	{
+		m_DebugController->EnableDebugLayer();
+		Logger::Log("Debug Layer Enabled!");
+		return true;
+	}
+
+	return false;
 }
 
 bool Renderer::CreateFactory()
@@ -123,7 +179,7 @@ bool Renderer::CreateSwapChain()
 	desc.Stereo = FALSE;
 	desc.SampleDesc = { 1, 0 };
 	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	desc.BufferCount = 2;
+	desc.BufferCount = FrameCount;
 
 	desc.Scaling = DXGI_SCALING_NONE;
 	desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
@@ -151,10 +207,11 @@ bool Renderer::CreateSwapChain()
 
 bool Renderer::CreateRTVDescriptorHeap()
 {
+	// Create a descriptor heap for RTV
 	D3D12_DESCRIPTOR_HEAP_DESC desc = {};
 
 	desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-	desc.NumDescriptors = 2;
+	desc.NumDescriptors = FrameCount;
 
 	bool ok = SUCCEEDED(m_Device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&m_RTVDescriptorHeap)));
 
@@ -172,11 +229,14 @@ bool Renderer::CreateRTVDescriptorHeap()
 
 bool Renderer::CreateRTV()
 {
-	auto rtvDescriptorSize = m_Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	// Take descriptor size (different depending on GPU/driver)
+	m_RTVDescriptorSize = m_Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
+	// Take CPU handle at the beginning
 	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_RTVDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
 
-	for (int i = 0; i < 2; ++i)
+	// Create RTV for each back buffer
+	for (int i = 0; i < FrameCount; ++i)
 	{
 		if (FAILED(m_SwapChain->GetBuffer(i, IID_PPV_ARGS(&m_BackBuffers[i]))))
 		{
@@ -184,7 +244,7 @@ bool Renderer::CreateRTV()
 			return false;
 		}
 		m_Device->CreateRenderTargetView(m_BackBuffers[i].Get(), nullptr, rtvHandle);
-		rtvHandle.Offset(rtvDescriptorSize);
+		rtvHandle.Offset(m_RTVDescriptorSize);
 	}
 
 	Logger::Log("RTVs Created");
@@ -195,7 +255,9 @@ bool Renderer::CreateCommandAllocators()
 {
 	bool ok = true;
 
-	for (int i = 0; i < 2; ++i)
+	// Create command allocators for each FrameCount to avoid resetting it while the GPU might use commands from it
+
+	for (int i = 0; i < FrameCount; ++i)
 	{
 		ok = ok && SUCCEEDED(m_Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_CommandAllocators[i])));
 	}
@@ -226,4 +288,40 @@ bool Renderer::CreateCommandList()
 	}
 
 	return ok;
+}
+
+bool Renderer::CreateFence()
+{
+	if (FAILED(m_Device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_Fence))))
+	{
+		return false;
+	}
+
+	// set 1 as starting sync point (value)
+	m_FenceValue = 1;
+
+	// windows event handle
+	m_FenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+
+	Logger::Log("Fence + Event Created!");
+
+	return m_FenceEvent != nullptr;
+}
+
+void Renderer::WaitForGPU()
+{
+	// tell the gpu queue when it reaches this value that the cpu can continue after that
+	m_CommandQueue->Signal(m_Fence.Get(), m_FenceValue);
+
+	// check if the gpu has used this frame - if yes -> ensure it has finished
+	if (m_Fence->GetCompletedValue() < m_FenceValue)
+	{
+		// signal when fence reaches a value
+		m_Fence->SetEventOnCompletion(m_FenceValue, m_FenceEvent);
+		// CPU blocked while GPU reaches that value
+		WaitForSingleObject(m_FenceEvent, INFINITE);
+	}
+
+	// start a new frame
+	m_FenceValue++;
 }
--- Engine/Renderer.h
+++ Engine/Renderer.h
@@ -23,20 +23,30 @@ private:
 	uint32_t m_Height = 720;
 
 	// DirectX 12
+	ComPtr<ID3D12Debug> m_DebugController;
 	ComPtr<IDXGIFactory6> m_Factory;
 	ComPtr<IDXGIAdapter4> m_Adapter;
 	ComPtr<ID3D12Device4> m_Device;
 
+	uint32_t m_CurrentFrameIndex = 0;
+	static const uint32_t FrameCount = 2;
+
 	ComPtr<ID3D12CommandQueue> m_CommandQueue;
-	ComPtr<ID3D12CommandAllocator> m_CommandAllocators[2];
+	ComPtr<ID3D12CommandAllocator> m_CommandAllocators[FrameCount];
 	ComPtr<ID3D12GraphicsCommandList> m_CommandList;
 
-	ComPtr<ID3D12Resource> m_BackBuffers[2];
+	ComPtr<ID3D12Resource> m_BackBuffers[FrameCount];
 	ComPtr<IDXGISwapChain4> m_SwapChain;
 
 	ComPtr<ID3D12DescriptorHeap> m_RTVDescriptorHeap;
+	uint32_t m_RTVDescriptorSize = 0;
+
+	ComPtr<ID3D12Fence> m_Fence;
+	uint64_t m_FenceValue = 0;
+	HANDLE m_FenceEvent;
 
 private:
+	bool EnableDebugLayer();
 	bool CreateFactory();
 	bool PickAdapter();
 	bool CreateDevice();
@@ -48,4 +58,10 @@ private:
 
 	bool CreateCommandAllocators();
 	bool CreateCommandList();
+
+	bool CreateFence();
+	void WaitForGPU();
+
+	void PreRender();
+	void PostRender();
 };
